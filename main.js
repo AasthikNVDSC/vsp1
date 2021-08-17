e3fdc582ce0351d3bdd29d6b960e1e287ae883f6
AasthikNVDSC <80753867+AasthikNVDSC@users.noreply.github.com>
2021-08-17 16:00:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var first_name = "Aasthik";
+var last_name = "N.V.D.S.C";
+console.log (first_name);
+console.log(last_name);
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+var word1 = "ENVIRONMENT";
+var word2 = "pollution";
+var word1_to_lowercase = word1.toLowerCase();
+console.log(word1_to_lowercase);
+var word2_to_uppercase = word2.toUpperCase();
+console.log(word2_to_uppercase);
+var text = "My name is N.V.D.S.C";
+var text_replace = text.replace("N.V.D.S.C","Aasthik")
+console.log(text_replace)
+var sqrt = Math.sqrt(9);
+console.log(sqrt);
+var max = Math.max(12,30);
+console.log(max)
+var min = Math.min(12,30);
+console.log(min)
+var a = 15 ;
+var b = 5 ;
+console.log(a)
+console.log(b)
+var add = a+b;
+var subtract = a-b;
+var multiply = a*b;
+console.log(add)
+console.log(subtract)
+console.log(multiply)
